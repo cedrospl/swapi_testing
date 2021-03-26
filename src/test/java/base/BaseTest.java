4169839a35820069a58fb041ca910bd007968f67
Mariusz Cz <hagnar93@gmail.com>
2021-03-26 21:41:23 +0100

    Added BaseTest.java file + new test to filmsTest.java

---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,7 @@
+package base;
+
+public class BaseTest {
+    protected static final String BASE_URL = "https://swapi.py4e.com/api/";
+    protected static final String FILMS_ENDPOINT = "films/";
+
+}
--- src/test/java/swapi/filmsTest.java
+++ src/test/java/swapi/filmsTest.java
@@ -1,31 +1,49 @@
 package swapi;
 
+import base.BaseTest;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.*;
 
-public class filmsTest {
+public class filmsTest extends BaseTest {
 
     @Test
-    public void readAllFilms(){
+    public void readAllFilms() {
 
         Response response = given()
                 .when()
-                .get("https://swapi.py4e.com/api/films")
+                .get(BASE_URL + FILMS_ENDPOINT)
                 .then()
                 .statusCode(HttpStatus.SC_OK)
                 .extract()
                 .response();
 
         JsonPath json = response.jsonPath();
-        System.out.println(json.prettyPrint());
+//        System.out.println(json.prettyPrint());
         List<String> episodesNumber = json.getList("results.episode_id");
-        Assertions.assertThat(episodesNumber.size()).isEqualTo(7);
+        assertThat(episodesNumber.size()).isEqualTo(7);
+    }
+
+    @Test
+    public void readOneFilm() {
+
+        Response response = given()
+                .pathParam("id", "5")
+                .when()
+                .get(BASE_URL + FILMS_ENDPOINT + "{id}")
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .extract()
+                .response();
+
+        JsonPath json = response.jsonPath();
+        assertThat(json.getString("title")).isEqualTo("Attack of the Clones");
+        assertThat(json.getString("release_date")).isEqualTo("2002-05-16");
     }
 }

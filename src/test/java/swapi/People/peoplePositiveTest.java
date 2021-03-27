362a1282674ae7be7ef6ca90ca5f29aa3785b06b
Mariusz Cz <hagnar93@gmail.com>
2021-03-27 15:49:32 +0100

    Added tests for /people/ endpoint

---
+++ src/test/java/swapi/People/peoplePositiveTest.java
@@ -0,0 +1,50 @@
+package swapi.People;
+
+import base.BaseTest;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.*;
+
+public class peoplePositiveTest extends BaseTest {
+
+    @Test
+    public void readAllPeople() {
+
+        Response response = given()
+                .spec(reqSpec)
+                .when()
+                .get(BASE_URL + PEOPLE_ENDPOINT)
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .extract()
+                .response();
+
+        JsonPath json = response.jsonPath();
+        assertThat(json.getString("count")).isEqualTo("87");
+    }
+
+    @Test
+    public void readOnePerson() {
+
+        Response response = given()
+                .spec(reqSpec)
+                .pathParam("id", "55")
+                .when()
+                .get(BASE_URL + PEOPLE_ENDPOINT + "{id}")
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .extract()
+                .response();
+
+        JsonPath json = response.jsonPath();
+        assertThat(json.getString("name")).isEqualTo("Adi Gallia");
+        assertThat(json.getString("birth_year")).isEqualTo("unknown");
+        assertThat(json.getString("gender")).isEqualTo("female");
+    }
+}

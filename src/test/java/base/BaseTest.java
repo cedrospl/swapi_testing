f8319cf322f36100dca6fdc1ad971aa80e03a569
Mariusz Cz <hagnar93@gmail.com>
2021-03-27 13:52:50 +0100

    Further development of BaseTest.java file

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,7 +1,29 @@
 package base;
 
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeAll;
+
 public class BaseTest {
     protected static final String BASE_URL = "https://swapi.py4e.com/api/";
     protected static final String FILMS_ENDPOINT = "films/";
+    protected static final String PEOPLE_ENDPOINT = "people/";
+    protected static final String PLANETS_ENDPOINT = "planets/";
+    protected static final String SPECIES_ENDPOINT = "species/";
+    protected static final String STARSHIPS_ENDPOINT = "starships/";
+    protected static final String VEHICLES_ENDPOINT = "vehicles/";
 
+    protected static RequestSpecBuilder reqBuilder;
+    protected static RequestSpecification reqSpec;
+
+    @BeforeAll
+    public static void beforeAll() {
+        reqBuilder = new RequestSpecBuilder();
+        reqBuilder.setContentType(ContentType.JSON);
+        reqBuilder.addFilter(new AllureRestAssured());
+
+        reqSpec = reqBuilder.build();
+    }
 }

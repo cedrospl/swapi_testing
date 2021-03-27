0271b60e651aedaeb60b66c96ff98c2a14ee3b6e
Mariusz Cz <hagnar93@gmail.com>
2021-03-27 13:53:43 +0100

    Addition of new tests in filmsNegativeTest.java file

---
+++ src/test/java/swapi/Films/filmsNegativeTest.java
@@ -0,0 +1,51 @@
+package swapi.Films;
+
+import base.BaseTest;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class filmsNegativeTest extends BaseTest {
+
+    @Test
+    public void addNewFilmWithoutBody() {
+        given()
+                .spec(reqSpec)
+                .when()
+                .post(BASE_URL + FILMS_ENDPOINT)
+                .then()
+                .statusCode(HttpStatus.SC_METHOD_NOT_ALLOWED);
+    }
+
+    @Test
+    public void update1FilmWithoutBody() {
+        given()
+                .spec(reqSpec)
+                .when()
+                .put(BASE_URL + FILMS_ENDPOINT)
+                .then()
+                .statusCode(HttpStatus.SC_METHOD_NOT_ALLOWED);
+    }
+
+    @Test
+    public void update2FilmWithoutBody() {
+        given()
+                .spec(reqSpec)
+                .when()
+                .patch(BASE_URL + FILMS_ENDPOINT)
+                .then()
+                .statusCode(HttpStatus.SC_METHOD_NOT_ALLOWED);
+    }
+
+    @Test
+    public void deleteExistingFilm() {
+        given()
+                .spec(reqSpec)
+                .pathParam("id", 1)
+                .when()
+                .delete(BASE_URL + FILMS_ENDPOINT + "{id}")
+                .then()
+                .statusCode(HttpStatus.SC_MOVED_PERMANENTLY);
+    }
+}
